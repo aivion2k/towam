70be5a55d599bc4194facf5ea98fdefa83f938e7
Michal <mkrol2k@gmail.com>
2021-10-24 15:27:02 +0200

    xd

--- igscrap.py
+++ igscrap.py
@@ -2,20 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-PATH = "C:/Program Files (x86)/chromedriver_win32/chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-driver.get("https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl")
-accept = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span")
-accept.click()
 
 
-watchers = driver.find_elements(By.ID,"video-title")
-wyswietlenia = driver.find_elements(By.CLASS_NAME,"style-scope ytd-video-meta-block")
-for i in range(len(watchers)):
+def open(string_path):
+    PATH = "C:/Program Files (x86)/chromedriver_win32/chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+
+    #if type(string_path) == str:
+    #    path = 'https://www.youtube.com/'+string_path
+    #    driver.get(path)
     
-    print(watchers[i].text)
-    print(wyswietlenia[i].text)
+    driver.get("https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl")
+    accept = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span")
+    accept.click()
+
+    watchers = driver.find_elements(By.ID,"video-title")
+    wyswietlenia = driver.find_elements(By.CLASS_NAME,"style-scope ytd-video-meta-block")
+    for i in range(len(watchers)):
+    
+        print(watchers[i].text)
+        print(wyswietlenia[i].text)
+
+
+    
+
+
+
+
+
 
 
 #search = driver.find_element_by_id()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import igscrap
 
 root = Tk()
 
@@ -12,6 +13,8 @@ root.resizable(False,False)
 def openNewWindow():
     newWindow = Toplevel(root)
     newWindow.title("New Window")
+    igscrap.open(entry_text.get())
+
 
 
 frm = ttk.Frame(root,padding=100,height=30,width=30)
@@ -22,15 +25,15 @@ lbl.config(font=('Arial',30))
 lbl.grid(column=0, row=0)
 
 
-
-textbox = ttk.Entry(frm)
+entry_text = StringVar()
+textbox = ttk.Entry(frm, textvariable=entry_text)
 textbox.grid(column=0,row=1,pady=10)
 
 
 btn = ttk.Button(frm, text="Analyse",command=openNewWindow)
 btn.grid(column=0, row=2)
 
-lbl1 = ttk.Label(frm,text="Leave blank to analyse /trending or type your youtubers' nickname")
+lbl1 = ttk.Label(frm,text="Leave blank to analyse /trending or type your youtuber's nickname")
 lbl1.config(font=('Arial',7))
 lbl1.grid(row=3)
 

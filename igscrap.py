3ac754b41aba1af0ec5fcb3568d946c9c784686e
Michal <mkrol2k@gmail.com>
2021-10-25 09:19:32 +0200

    towam

--- igscrap.py
+++ igscrap.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from bs4 import BeautifulSoup
 
 
 def open(string_path):
@@ -15,13 +14,26 @@ def open(string_path):
     driver.get("https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl")
     accept = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span")
     accept.click()
-
-    watchers = driver.find_elements(By.ID,"video-title")
-    wyswietlenia = driver.find_elements(By.CLASS_NAME,"style-scope ytd-video-meta-block")
-    for i in range(len(watchers)):
     
-        print(watchers[i].text)
-        print(wyswietlenia[i].text)
+    content = driver.page_source
+    soup = BeautifulSoup(content,'html.parser')
+    titles = soup.find_all('a', {'id':'video-title'})
+    authors = soup.find_all('div', {'id':'metadata-line'},{'class':'style-scope ytd-video-meta-block'})
+    for i in range(len(titles)):
+    
+        print(authors[i].text,titles[i].text)
+        
+
+  
+    
+
+    
+    #watchers = driver.find_elements(By.ID,"video-title")
+    #wyswietlenia = driver.find_elements(By.CLASS_NAME,"style-scope ytd-video-meta-block")
+    #for i in range(len(watchers)):
+    
+    #    print(watchers[i].text)
+    #    print(wyswietlenia[i].text)
 
 
     
--- main.py
+++ main.py
@@ -2,42 +2,53 @@ from tkinter import *
 from tkinter import ttk
 import igscrap
 
+class MyApp():
+    def __init__(self,root):
+        style = ttk.Style(root)
+        current_theme = style.theme_use('clam')
+
+        root.title('YT analyser')
+        root.resizable(False,False)
+
+        self.frm = ttk.Frame(root,padding=100,height=30,width=30)
+        self.frm.grid()
+
+        lbl = ttk.Label(self.frm, text="YouTube analyser",padding=10)
+        lbl.config(font=('Arial',30))
+        lbl.grid(column=0, row=0)
+
+
+        self.entry_text = StringVar()
+        textbox = ttk.Entry(self.frm, textvariable=self.entry_text)
+        textbox.grid(column=0,row=1,pady=10)
+
+
+        btn = ttk.Button(self.frm, text="Analyse",command=self.openNewWindow)
+        btn.grid(column=0, row=2)
+
+        lbl1 = ttk.Label(self.frm,text="Leave blank to analyse /trending or type your youtuber's nickname")
+        lbl1.config(font=('Arial',7))
+        lbl1.grid(row=3) 
+
+        self.pgb = ttk.Progressbar(self.frm,orient='horizontal',mode='indeterminate',length=280)        
+
+        
+
+
+    def openNewWindow(self):
+        self.pgb.grid(row=4,pady=10)
+        self.pgb.start()
+        newWindow = Toplevel(root)
+        newWindow.title("New Window")
+        igscrap.open(self.entry_text.get())
+
+    def hide_button(self,widget):
+    # This will remove the widget from toplevel
+        widget.pack_forget()
+
+
+
+
 root = Tk()
-
-style = ttk.Style(root)
-current_theme = style.theme_use('clam')
-
-root.title('YT analyser')
-root.resizable(False,False)
-
-def openNewWindow():
-    newWindow = Toplevel(root)
-    newWindow.title("New Window")
-    igscrap.open(entry_text.get())
-
-
-
-frm = ttk.Frame(root,padding=100,height=30,width=30)
-frm.grid()
-
-lbl = ttk.Label(frm, text="YouTube analyser",padding=10)
-lbl.config(font=('Arial',30))
-lbl.grid(column=0, row=0)
-
-
-entry_text = StringVar()
-textbox = ttk.Entry(frm, textvariable=entry_text)
-textbox.grid(column=0,row=1,pady=10)
-
-
-btn = ttk.Button(frm, text="Analyse",command=openNewWindow)
-btn.grid(column=0, row=2)
-
-lbl1 = ttk.Label(frm,text="Leave blank to analyse /trending or type your youtuber's nickname")
-lbl1.config(font=('Arial',7))
-lbl1.grid(row=3)
-
-
-
-
+MyApp(root)
 root.mainloop()
